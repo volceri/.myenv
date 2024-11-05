36c75d6ae55ff4f1d5db8e34bb0e071c81d0dfd0
Volceri <volceri.davila@gmail.com>
2024-11-05 22:13:39 +0000

    Adding X11 LXQT desktop manager

--- system/wm/x11_lxqt.nix
+++ system/wm/x11_lxqt.nix
@@ -8,20 +8,26 @@
 
   # Configure X11
   services.xserver = {
-    enable = true;
-    layout = "us";
-    #xkbVariant = "";
+    enable = true;    
     #xkbOptions = "caps:escape";
     excludePackages = [ pkgs.xterm ];
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
     displayManager = {
       lightdm.enable = true;
       #sessionCommands = ''
-      #xset -dpms
-      #xset s blank
-      #xset r rate 350 50
-      #xset s 300
-      #${pkgs.lightlocker}/bin/light-locker --idle-hint &
-    #'';
+        #xset -dpms
+        #xset s blank
+        #xset r rate 350 50
+        #xset s 300
+        #${pkgs.lightlocker}/bin/light-locker --idle-hint &
+      #'';    
+    };
+    desktopManager = {
+      # Enable the LXQT Desktop Environment.
+      lxqt.enable = true;
     };
     libinput = {
       #touchpad.disableWhileTyping = true;
