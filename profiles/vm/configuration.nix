5e52a8ff101a46affb8a22c3ca8183c7c19966ba
Volceri <volceri.davila@gmail.com>
2024-11-05 00:31:46 +0000

    Fixing hardware config import in vm profile

--- profiles/vm/configuration.nix
+++ profiles/vm/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ../../hardware-configuration.nix
+      ../../system/hardware-configuration.nix
       ../../system/security/gpg.nix
     ];
 
