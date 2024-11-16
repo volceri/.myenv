789ab469a4de74626ea93c600d6e7bf23f341706
Volceri D Avila <volceri.avila@olx.com>
2024-11-16 19:19:21 +0000

    Trying to fix fonts

--- home/wm/fonts.nix
+++ home/wm/fonts.nix
@@ -1,25 +1,12 @@
-# { pkgs, ... }: {
-#   fonts.packages = with pkgs; [
-#     nerdfonts
-#     powerline
-#     fira-code
-#     fira-code-symbols
-#     fira-code-nerdfont
-#     font-manager
-#     font-awesome_5
-#     noto-fonts
-#   ];
-# }
-
-
-{ pkgs, ... }:
-
-{
-  # Fonts are nice to have
-  fonts.packages = with pkgs; [
-    # Fonts
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
     nerdfonts
     powerline
+    fira-code
+    fira-code-symbols
+    fira-code-nerdfont
+    font-manager
+    font-awesome_5
+    noto-fonts
   ];
-
 }
