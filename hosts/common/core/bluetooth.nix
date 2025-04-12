83fbbf416320a2999d6be4e2e1b40b82564fc309
Volceri D'Avila <volceri.davila@gmail.com>
2025-04-12 18:36:13 +0100

    Removing blueman service (using plasma6)

--- hosts/common/core/bluetooth.nix
+++ hosts/common/core/bluetooth.nix
@@ -1,5 +1,5 @@
 {...}:{
     hardware.bluetooth.enable = true; # enables support for Bluetooth
     hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-    services.blueman.enable = true;
+    # services.blueman.enable = true;
 }
